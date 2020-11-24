c6e44203591ab1c3b5b9abc3153588fa504c09d1
Sandra Olsson <sandra.olsson@jayway.com>
2020-11-24 12:28:40 +0100

    adding gif to Question component & updating questions.json to hold gif info

--- src/Views/QuestionsView/Components/Question.jsx
+++ src/Views/QuestionsView/Components/Question.jsx
@@ -1,31 +1,45 @@
 import { React, useState } from 'react';
 import questions from '../../../questions.json';
+import './Question.css';
 
-export const Question = ({nextView}) => {
+export const Question = () => {
 
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const currentQuestion = questions[currentQuestionIndex].question;
-  const questionAlternatives = questions[currentQuestionIndex].alternatives;
+  const question = questions[currentQuestionIndex];
+  const currentQuestion = question.question;
+  const questionAlternatives = question.alternatives;
+
+  //gif
+  const gifLink = question.gif.gifLink;
+  const gifLinkEmbedded = question.gif.gifLinkEmbedded;
+  const gifTitle = question.gif.title;
   
   const nextQuestion = () => {
-    if (currentQuestionIndex === questions.length - 1) {
-      nextView()
-      return
-    }
     setCurrentQuestionIndex(currentQuestionIndex + 1)
   }
-
+  console.log(question);
   console.log(currentQuestion);
   console.log(currentQuestionIndex);
   console.log(questionAlternatives);
 
   return(
-    <main className="Question">
+    <section className="Question">
       <div className="question__container">
         <h1 className="question__title">Question {currentQuestionIndex +1}</h1>
         <p className="question__question">
           {currentQuestion}
         </p>
+        <div className="question__gif-container">
+          <iframe 
+            src={gifLinkEmbedded}
+            title={gifTitle}  
+            frameBorder="0" 
+            className="question__giphy-embed" 
+          ></iframe>
+          <p>
+            <a href={gifLink} rel="noreferrer" target="_blank">via GIPHY</a>
+          </p>
+        </div>
         <div className="question__alternative-container">
           <button className="question__alternative">{questionAlternatives[0].alternative}</button>
           <button className="question__alternative">{questionAlternatives[1].alternative}</button>
@@ -34,6 +48,6 @@ export const Question = ({nextView}) => {
         </div>
         <button className="question__next" onClick={nextQuestion}>Next question</button>
       </div>
-    </main>
+    </section>
   )
 }
--- src/Views/QuestionsView/QuestionsView.jsx
+++ src/Views/QuestionsView/QuestionsView.jsx
@@ -1,10 +1,11 @@
 import { React} from 'react';
 import { Question } from './Components/Question';
+import './QuestionsView.css';
 
 export const QuestionsView = ({nextView}) => {
 
   return(
-    <main>
+    <main className="Questions-view">
       <Question />
       <button onClick={nextView}>Go to results</button>
     </main>
