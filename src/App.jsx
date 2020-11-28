79bc856efa0192a7eb32162e1c8e0a45ac620603
Sandra Olsson <sandra.olsson@jayway.com>
2020-11-28 23:03:53 +0100

    updating tenRandomQuestions to get ten new random questions when retaking the quiz

--- src/App.jsx
+++ src/App.jsx
@@ -5,46 +5,46 @@ import { QuestionsView } from './Views/QuestionsView/QuestionsView';
 import { ResultView } from './Views/ResultView/ResultView';
 import questions from './questions.json';
 
+const askedQuestions = [];
+
+const getRandomQuestion = () => {
+  const randomQuestionIndex = Math.floor(Math.random() * questions.length);
+  const randomQuestion = questions[randomQuestionIndex];
+  if (askedQuestions.includes(randomQuestion)) {
+    if (askedQuestions.length === questions.length) {
+      console.log('No more questions left!');
+      return;
+    }
+    return getRandomQuestion();
+  }
+  askedQuestions.push(randomQuestion);
+  return randomQuestion;
+}
+
+const getTenRandomQuestions = () => {
+  const tenRandomQuestions = [];
+  for (let i = 0; i < 10; i++) {
+    tenRandomQuestions.push(getRandomQuestion());
+  }
+  return tenRandomQuestions;
+}
+
+const tenRandomQuestionsInit = getTenRandomQuestions();
 
 function App() {
   const [currentViewIndex, setCurrentViewIndex] = useState(0);
   const [numOfCorrectAnswers, setNumOfCorrectAnswers] = useState(0);
+  const [tenRandomQuestions, setTenRandomQuestions] = useState(tenRandomQuestionsInit); 
 
   const nextView = () => {
     if (currentViewIndex === views.length - 1) {
       setCurrentViewIndex(0);
+      setTenRandomQuestions(getTenRandomQuestions());
       return
     }
     setCurrentViewIndex(currentViewIndex + 1);
   }
-
-  const askedQuestions = [];
-
-  const getRandomQuestion = () => {
-    const randomQuestionIndex = Math.floor(Math.random() * questions.length);
-    const randomQuestion = questions[randomQuestionIndex];
-    if (askedQuestions.includes(randomQuestion)) {
-      if (askedQuestions.length === questions.length) {
-        console.log('No more questions left!');
-        return;
-      }
-      return getRandomQuestion();
-    }
-    askedQuestions.push(randomQuestion);
-    return randomQuestion;
-  }
-
-  const getTenRandomQuestions = () => {
-    const tenRandomQuestions = [];
-    for (let i = 0; i < 10; i++) {
-      tenRandomQuestions.push(getRandomQuestion());
-    }
-    return tenRandomQuestions;
-  }
-
-  const [tenRandomQuestions] = useState(getTenRandomQuestions()); 
   
-
   const increaseScore = (isCorrect) => {
     if (isCorrect) {
       setNumOfCorrectAnswers(numOfCorrectAnswers + 1);
