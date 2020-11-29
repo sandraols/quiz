71bebdac4e7043b5726a316cacff5ba212cc2817
Sandra Olsson <sandra.olsson@jayway.com>
2020-11-29 20:57:51 +0100

    adding styling and gif to ResultView

--- src/ViewComponents/ResultView/ResultView.jsx
+++ src/ViewComponents/ResultView/ResultView.jsx
@@ -1,15 +1,27 @@
 import React from 'react';
+import './ResultView.css';
 
 export const ResultView = ({ nextView, numOfCorrectAnswers, numOfIncorrectAnswers, numOfUnansweredQuestions }) => {
 
   return(
-    <main>
-      <h1>The Results</h1>
-      <p>You scored {numOfCorrectAnswers} points</p>
-      <p>You have {numOfIncorrectAnswers} incorrect answers</p>
-      <p>You have {numOfUnansweredQuestions} unanswered questions</p>
-      
-      <button onClick={nextView}>Retake Quiz</button>
+    <main className="Result-view">
+      <div className="result-view__container">
+        <h1>The Result</h1>
+        <p>You scored {numOfCorrectAnswers} points</p>
+        <p>You had {numOfIncorrectAnswers} incorrect answers</p>
+        <p>You had {numOfUnansweredQuestions} unanswered questions</p>
+        <iframe 
+          src="https://giphy.com/embed/zGnnFpOB1OjMQ" 
+          frameBorder="0" 
+          className="result-view__giphy-embed" 
+          title="Hobbits from Lord of The Rings applauses"
+        ></iframe>
+        <p>
+          <a rel="noreferrer" target="_blank" href="https://giphy.com/gifs/lord-of-the-rings-hobbits-shire-zGnnFpOB1OjMQ">via GIPHY</a>
+        </p>
+
+        <button className="result-view__button" onClick={nextView}>Retake Quiz</button>
+      </div>
     </main>
   )
 }
